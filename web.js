330b216266c48dc2a17f371780bf6f8f3d86d47b
Sowndharya1009 <sowndharyamanoharan143@gmail.com>
2024-12-14 21:56:50 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,14 @@
+function change()
+{
+    alert("Sumbitted successfully");
+}
+function  modify()
+{
+    document.getElementById("myForm").reset();
+}
+ var image1 = document.getElementById("img1")
+
+ function color()
+ {
+    image1.style.backgroundColor="skyblue"
+ }
